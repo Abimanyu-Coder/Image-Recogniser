cda36b24669a3f2a2def5732a8e51a10cac60f49
Abimanyu-Coder <91654799+Abimanyu-Coder@users.noreply.github.com>
2022-09-05 19:34:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+webcam.attach( '#camera' );
+
+function
+
